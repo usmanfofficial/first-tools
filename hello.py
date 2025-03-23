a5ce8732e703455fd1d3adb60be666700be8be30
Usman Farooq <usmanf0601@gmail.com>
2025-03-23 01:54:07 +0000

    Completed function

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
 def greet(name):
+    """
+        Gretting user with personalised message
+    """
     return f"Hello, {name}! Welcome to my first GitHub repo."
 
-if __name__ == "__main__":
+def main():
     user = input("Enter your name: ")
     print(greet(user))
+
+if __name__ == "__main__":
+    main()
