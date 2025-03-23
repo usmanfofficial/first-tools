e1cd24e6901b1f4f007422bd98089bb00d1f0438
Usman Farooq <usmanf0601@gmail.com>
2025-03-23 04:22:29 +0000

    Added a bunch of quotes

--- quote_generator.py
+++ quote_generator.py
@@ -1,1 +1,14 @@
 import random  
+
+quotes = [
+    "Believe you can and you're halfway there.",  # Quote by Theodore Roosevelt
+    "The only way to do great work is to love what you do.",  # Quote by Steve Jobs
+    "Success is not final, failure is not fatal: It is the courage to continue that counts.",  # Quote by Winston Churchill
+    "Do what you can, with what you have, where you are.",  # Quote by Theodore Roosevelt
+    "It does not matter how slowly you go as long as you do not stop.",  # Quote by Confucius
+    "Hardships often prepare ordinary people for an extraordinary destiny.",  # Quote by C.S. Lewis
+    "Dream big and dare to fail.",  # Quote by Norman Vaughan
+    "Opportunities don't happen, you create them."  # Quote by Chris Grosser
+    "If you have a one percent chance, try 100 times.” # Quote by Saygin Yalcin "
+
+]
