796bfdebd35760e702f81b0de7de66a6390e767a
Usman Farooq <usmanf0601@gmail.com>
2025-04-02 11:58:44 +0000

    Added expection value

--- calculator.py
+++ calculator.py
@@ -59,4 +59,5 @@ def calculator():
             else:
                 print("Invalid choice. Please try again.")
         
-
+        except ValueError:
+            print("\nError: Invalid input. Please enter numeric values.")
