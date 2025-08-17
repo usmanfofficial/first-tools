74db1921a6196443e1157c4956e9752043d250db
usmanfofficial <usmanf0601@gmail.com>
2025-08-17 00:11:06 -0400

    Add function

--- projects/todo_cli/app.py
+++ projects/todo_cli/app.py
@@ -1,2 +1,20 @@
 import sys
 import core
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python app.py [add <title>] [list]")
+        return
+
+    cmd = sys.argv[1]
+
+    if cmd == "add":
+        title = " ".join(sys.argv[2:])
+        task = core.add_task(title)
+        print(f"Added: {task.title} (id {task.id})")
+
+    elif cmd == "list":
+        tasks = core.list_tasks()
+        for t in tasks:
+            mark = "[x]" if t.done else "[ ]"
+            print(f"{mark} {t.id}: {t.title}")
