a32cabf3b2b843c22e7506aad37e6b248c4d5335
usmanfofficial <usmanf0601@gmail.com>
2025-08-17 00:16:49 -0400

    Add storage with save_tasks/load_tasks (JSON)

--- projects/todo_cli/storage.py
+++ projects/todo_cli/storage.py
@@ -1,15 +1,13 @@
+# projects/todo_cli/storage.py
 import json
-
-from model import Task
+from .model import Task
 
 def save_tasks(tasks, filename="tasks.json"):
-    """Save tasks to a JSON file."""
-    data = [task.__dict__ for task in tasks]
+    data = [t.__dict__ for t in tasks]
     with open(filename, "w") as f:
         json.dump(data, f, indent=2)
 
 def load_tasks(filename="tasks.json"):
-    """Load tasks from a JSON file."""
     try:
         with open(filename, "r") as f:
             data = json.load(f)
